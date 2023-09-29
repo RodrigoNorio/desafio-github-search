1115ee66e95e1ce2e5181ef1cad901870e9accf1
unknown <norio.rnt@gmail.com>
2023-09-29 20:40:11 -0300

    add empty state

--- app/src/main/java/br/com/igorbag/githubsearch/ui/MainActivity.kt
+++ app/src/main/java/br/com/igorbag/githubsearch/ui/MainActivity.kt
@@ -8,8 +8,12 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import br.com.igorbag.githubsearch.R
 import br.com.igorbag.githubsearch.data.GitHubService
@@ -28,6 +32,9 @@ class MainActivity : AppCompatActivity() {
   lateinit var btnConfirm: Button
   lateinit var repoList: RecyclerView
   lateinit var githubApi: GitHubService
+  lateinit var imgNoConnection: ImageView
+  lateinit var txtNoConnection: TextView
+  lateinit var progressBar: ProgressBar
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -36,13 +43,20 @@ class MainActivity : AppCompatActivity() {
     setupListeners()
     showUserName()
     setupRetrofit()
+  }
+
+  override fun onResume() {
+    super.onResume()
     getAllReposByUserName()
   }
 
-  fun setupView() {
+  private fun setupView() {
     userName = findViewById(R.id.et_user_name)
     btnConfirm = findViewById(R.id.btn_confirm)
     repoList = findViewById(R.id.rv_repo_list)
+    imgNoConnection = findViewById(R.id.iv_no_connection)
+    txtNoConnection = findViewById(R.id.tv_no_connection)
+    progressBar = findViewById(R.id.pb_loader)
   }
 
   private fun setupListeners() {
@@ -55,7 +69,7 @@ class MainActivity : AppCompatActivity() {
   private fun clearFocus(view: View) {
     view.clearFocus()
     val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
-    imm.hideSoftInputFromWindow(view.getWindowToken(), 0)
+    imm.hideSoftInputFromWindow(view.windowToken, 0)
   }
   private fun saveUserLocal() {
     val inputName = userName.text.toString()
@@ -69,11 +83,11 @@ class MainActivity : AppCompatActivity() {
 
   private fun showUserName() {
     val sharedPref = getPreferences(Context.MODE_PRIVATE)
-    val saved_Name = sharedPref.getString(getString(R.string.saved_name), "")
-    userName.setText(saved_Name)
+    val savedName = sharedPref.getString(getString(R.string.saved_name), "")
+    userName.setText(savedName)
   }
 
-  fun setupRetrofit() {
+  private fun setupRetrofit() {
     val retrofit = Retrofit.Builder()
       .baseUrl("https://api.github.com/")
       .addConverterFactory(GsonConverterFactory.create())
@@ -82,29 +96,40 @@ class MainActivity : AppCompatActivity() {
     githubApi = retrofit.create(GitHubService::class.java)
   }
 
-  fun getAllReposByUserName() {
+  private fun getAllReposByUserName() {
     val userName = userName.text.toString()
     githubApi.getAllRepositoriesByUser(userName).enqueue(object: Callback<List<Repository>> {
       override fun onResponse(call: Call<List<Repository>>, response: Response<List<Repository>>) {
         if(response.isSuccessful) {
+          progressBar.isVisible = false
+          imgNoConnection.isVisible = false
+          txtNoConnection.isVisible = false
           response.body()?.let {
             setupAdapter(it)
           }
         } else {
-          Toast.makeText(this@MainActivity, "error on connection", Toast.LENGTH_SHORT).show()
+          Toast.makeText(this@MainActivity, "Something went wrong", Toast.LENGTH_SHORT).show()
         }
       }
 
       override fun onFailure(call: Call<List<Repository>>, t: Throwable) {
-        //emptyState()
-        Toast.makeText(this@MainActivity, "no connection", Toast.LENGTH_SHORT).show()
+        emptyState()
+        Toast.makeText(this@MainActivity, "No connection", Toast.LENGTH_SHORT).show()
       }
     })
   }
 
   fun setupAdapter(list: List<Repository>) {
+    repoList.isVisible = true
     val adapter = RepositoryAdapter(list)
     repoList.adapter = adapter
+  }
+
+  fun emptyState() {
+    progressBar.isVisible = false
+    repoList.isVisible = false
+    imgNoConnection.isVisible = true
+    txtNoConnection.isVisible = true
   }
 
 
--- app/src/main/java/br/com/igorbag/githubsearch/ui/adapter/RepositoryAdapter.kt
+++ app/src/main/java/br/com/igorbag/githubsearch/ui/adapter/RepositoryAdapter.kt
@@ -11,46 +11,25 @@ import br.com.igorbag.githubsearch.domain.Repository
 class RepositoryAdapter(private val repositories: List<Repository>) :
     RecyclerView.Adapter<RepositoryAdapter.ViewHolder>() {
 
-    // Cria uma nova view
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view =
             LayoutInflater.from(parent.context).inflate(R.layout.repository_item, parent, false)
         return ViewHolder(view)
     }
 
-    // Pega o conteudo da view e troca pela informacao de item de uma lista
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.tvRepoName.text = repositories[position].name
-        //@TODO 8 -  Realizar o bind do viewHolder
-        //Exemplo de Bind
-        //  holder.preco.text = repositories[position].atributo
-
-        // Exemplo de click no item
-        //holder.itemView.setOnClickListener {
-        // carItemLister(repositores[position])
-        //}
-
-        // Exemplo de click no btn Share
-        //holder.favorito.setOnClickListener {
-        //    btnShareLister(repositores[position])
-        //}
     }
 
-    // Pega a quantidade de repositorios da lista
-    //@TODO 9 - realizar a contagem da lista
     override fun getItemCount(): Int = repositories.size
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val tvRepoName: TextView = view.findViewById(R.id.tv_repo_name)
-        //@TODO 10 - Implementar o ViewHolder para os repositorios
-        //Exemplo:
-        //val atributo: TextView
-
-        //init {
-        //    view.apply {
-        //        atributo = findViewById(R.id.item_view)
-        //    }
-
+        val tvRepoName: TextView
+        init {
+          view.apply {
+              tvRepoName = findViewById(R.id.tv_repo_name)
+          }
+        }
     }
 }
 
